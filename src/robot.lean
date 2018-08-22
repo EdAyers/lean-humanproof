7836e3f8207b87fabafe8ece2701cadd4d328d60
E.W.Ayers <E.W.Ayers@maths.cam.ac.uk>
2018-08-22 01:57:54 +0100

    Fix `robot.lean`

--- src/robot.lean
+++ src/robot.lean
@@ -2,15 +2,15 @@
 Maybe this is a bad idea. But it will give me practice with typeclasses in Lean so it won't be a waste of time -/
 
 import meta.expr
-import exprset
-open tactic expr
+import table
+open tactic expr dict table
 universe u
 
 namespace robot
 
 meta inductive hyp_telescope
 -- prev are values that have been substituted for the binder in the past.
-|Take  (type : expr) (prev : exprset) (rest : hyp_telescope)
+|Take  (type : expr) (prev : table expr) (rest : hyp_telescope)
 |Give  (type : expr) (rest : hyp_telescope)
 |Premiss (prop: expr) (rest : hyp_telescope) 
 |Conclusion (prop : expr)
@@ -18,26 +18,26 @@ meta inductive hyp_telescope
 meta structure hyp :=
     (body : expr) -- the proof term for the hyp
     (type : hyp_telescope) -- the type has been 'preunwound' to make the conclusion more accessible.
-    (vuln : bool) -- if it's vulnerable, it's fair game for delete_dangling and delete_unmatchable
-    (refs : exprset) -- a table of local_consts that the type depends on.
+    (vuln : bool) -- if it is vulnerable then it is fair game for delete_dangling and delete_unmatchable
+    (refs : table expr) -- a table of local_consts that the type depends on.
 
-meta inductive bullet_type |Diamond | Circle
+meta inductive bullet_type |Diamond |Circle
 
 meta structure bullet :=
-    (dependent : exprset)
-    (independent : exprset)
+    (dependent : table expr) -- local-consts and metavariables that it depends on
+    (independent : table expr) -- local-consts and metavariables that it is not allowed to depend on
     (type : bullet_type)
 
-meta def hyps := rbmap expr hyp
+meta def hyps := dict expr hyp
 meta structure robot_state :=
     (vars : list expr)
     (hyps : hyps)
     (target : expr)
-    (bullets : rbmap expr bullet)
+    (bullets : dict expr bullet)
 
 -- Note that this is just `StateT robot_state tactic α` if we loved monad transformers.
 -- The idea is that `robot` wraps `tactic` to ensure that the extra state remains up to date with the underlying goal structure.
-@[reducible] meta def robot (α : Type u) := robot_state -> tactic (α × robot_state)
+@[reducible] meta def robot (α : Type u) := robot_state → tactic (α × robot_state)
 
 meta instance : monad robot :=
 { map := λ α β f x s, x s >>= (λ ⟨a,s⟩, pure ⟨f a, s⟩)
@@ -51,53 +51,53 @@ meta instance : alternative robot :=
 }
 
 
-private meta def mk_hyp_telescope : expr -> tactic hyp_telescope
+private meta def mk_hyp_telescope : expr → tactic hyp_telescope
 | (pi n bi a b) := do
     -- TODO; if `a` is a sigma or a product, convert to a Pi using Exists.elim
-    p <- is_prop a,
-    rest <- mk_hyp_telescope b,
-    return $ if p then hyp_telescope.Take a exprset.empty rest
-            else hyp_telescope.Premiss a rest
+    p ← is_prop a,
+    rest ← mk_hyp_telescope b,
+    return $ if p then hyp_telescope.Take a table.empty rest
+             else hyp_telescope.Premiss a rest
 | (app (app `(Exists) a) p) := do
-    p <- whnf p,
+    p ← whnf p,
     match p with
     |(pi n bi _ b) := do
-        rest  <- mk_hyp_telescope b,
+        rest ← mk_hyp_telescope b,
         return $ hyp_telescope.Give a rest
     |_ := none
     end
 | x := return $ hyp_telescope.Conclusion x 
 
-private meta def mk_hp_vars_and_hyps : (list expr) -> hyps -> (list expr) -> tactic ((list expr) × hyps)
+private meta def mk_hp_vars_and_hyps : (list expr) → hyps → (list expr) → tactic ((list expr) × hyps)
 |(vs) (hs) ([]) := return ⟨vs, hs⟩
 |(vs) (hs) (v :: t) := do
     -- TODO expand the definition of the type once?
-    type <- infer_type v,
-    isProp <- is_prop type,
+    type ← infer_type v,
+    isProp ← is_prop type,
     if (isProp) then do
-        refs <- return $ from_list $ expr.list_local_const $ type,
-        telescope <- mk_hyp_telescope type,
-        hyp <- return $ hyp.mk v telescope false refs,
-        mk_hp_vars_and_hyps vs (rbmap.insert hs v hyp) t 
+        refs ← return $ table.from_list $ expr.list_local_const $ type,
+        telescope ← mk_hyp_telescope type,
+        hyp ← return $ hyp.mk v telescope false refs,
+        mk_hp_vars_and_hyps vs (dict.insert v hyp hs) t 
     else mk_hp_vars_and_hyps (v :: vs) hs t
 
 private meta def mk_robot_state : tactic robot_state := do
-    ctx <- local_context,
-    t <- target,
-    ⟨vs, hs⟩ <- mk_hp_vars_and_hyps [] (mk_rbmap _ _) ctx,
-    return $ {vars := vs, hyps := hs, target := t, bullets := mk_rbmap _ _ }
+    ctx ← local_context,
+    t ← target,
+    ⟨vs, hs⟩ ← mk_hp_vars_and_hyps [] empty ctx,
+    return $ {vars := vs, hyps := hs, target := t, bullets := empty }
 
 meta def get_hyps : robot $ hyps := λ s, pure ⟨s.hyps, s⟩
-meta def set_hyps : hyps -> robot unit := λ hs s, pure ⟨(), {hyps := hs, ..s}⟩
+meta def set_hyps : hyps → robot unit := λ hs s, pure ⟨(), {hyps := hs, ..s}⟩
 meta def get_vars : robot $ list expr := λ s, pure ⟨s.vars, s⟩
-private meta def from_tactic {α : Type} : tactic α -> robot α := λ t s, t >>= λ a, pure ⟨a,s⟩
+private meta def from_tactic {α : Type} : tactic α → robot α := λ t s, t >>= λ a, pure ⟨a,s⟩
 meta instance {α : Type} : has_coe (tactic α) (robot α) := ⟨from_tactic⟩ 
 meta def run {α : Type} (r : robot α) : tactic α := prod.fst <$> (mk_robot_state >>= r)
 
 /--Remove a hypothesis. -/
 meta def clear_hyp (e : expr) : robot unit := do
-    hs <- get_hyps,
-    hs' <- pure $ rbmap (λ (h: hyp), e ≠ h.body) hs,
+    hs ← get_hyps,
+    hs' ← pure $ dict.filter (λ _ (h: hyp), decidable.to_bool $ e ≠ h.body) $ hs,
     clear e,
     set_hyps hs'
 
