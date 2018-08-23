43f98915c8044f73b0495c4ec93c1ede77476f59
E.W.Ayers <E.W.Ayers@maths.cam.ac.uk>
2018-08-23 12:50:34 +0100

    Stash

--- src/deletion.lean
+++ src/deletion.lean
@@ -3,53 +3,42 @@
 
 import meta.expr
 import table
+import robot
 open tactic expr
-
+open table robot
 universes u v w
 def list.mchoose {m : Type u → Type v} [monad m] {α : Type w} {β : Type u} (f : α → m (option β)) : list α → m (list β)
 | []       := return []
 | (h :: t) := pure (λ (h : option β) (t : list β), option.rec_on h t (λ h, h :: t)) <*> f h <*> list.mchoose t
 
-def list.some {α : Type u} (p : α -> bool) : list α -> bool
-| [] := false
-| (h :: t) := p h || list.some t
 
-meta def get_local_consts (e : expr) : table expr := from_list $ expr.list_local_const $ e
 
-meta structure formula :=
-(term : expr)
-(type : expr)
-(deps : table expr)
+meta def get_local_consts (e : expr) : table expr := table.from_list $ expr.list_local_const $ e
 
-meta def as_formula (h : expr) : tactic $ option formula :=
-do
-    y <- infer_type h,
-    p <- is_prop y,
-    let deps := get_local_consts y in
-    return $ if p then some ⟨h, y, deps⟩ else none
+/- We say that a statement is __dangling__ when it references a variable that no other statements reference. -/
 
-/--Get all of the context entries which are propositions along with their types.-/
-meta def local_hypotheses : tactic $ list formula :=
-local_context >>= list.mchoose as_formula
-
-/--Get the goals which are propositions along with their types -/
-meta def local_targets : tactic $ list formula :=
-get_goals >>= list.mchoose as_formula
-
-meta def find_dangling : table expr -> list formula -> list formula -> list formula
+/--Arguments:
+   - an accumulating table of local constants that all of the hyps considered so far.
+     add the references of all of the goals to this.
+   - an accumulator full of the statements that are dangling
+   - a queue of statements yet to check.
+   Returns: a list of dangling statements.
+-/
+meta def find_dangling : table expr → list hyp → list hyp → list hyp
 | d acc [] := acc
 | d acc  (h :: hs) :=
-    find_dangling (union d h.deps) (
-        if list.some (λ h, (not $ contains d h) && list.all hs (λ h', not $ contains h'.deps h)) $ to_list h.deps
+    find_dangling (d ∪ h.refs) (  
+        -- If there is a variable `x ∈ h.deps` that doesn't appear in any of these then add it to the list of condemned statements
+        if table.any (λ x, (not $ contains x d) && list.all hs (λ h', not $ contains x h'.refs)) h.refs 
         then h :: acc else acc
     ) (hs)
 
-meta def delete_dangling : tactic unit :=
+meta def delete_dangling : robot unit :=
 do
-    hyps <- local_hypotheses,
-    targets <- local_targets,
-    target_deps <- return $ list.foldl union empty $ list.map (λ t : formula, t.deps) targets,
-    list.mmap' (λ (h : formula), clear h.term) $ find_dangling target_deps [] hyps
+    hyps ← get_hyps,
+    targets ← get_targets,
+    target_deps ← return $ dict.fold (λ _, union) empty $ dict.map (statement.refs) targets,
+    list.mmap' (λ h : statement, clear h.body) $ find_dangling target_deps [] hyps
 
 meta def scratch_tac : tactic unit :=
 do v ← mk_meta_var `(ℕ),
@@ -58,7 +47,7 @@ do v ← mk_meta_var `(ℕ),
    skip
 
 variables a b c : Prop
-example : a -> (a -> b) -> c -> b :=
+example : a → (a → b) → c → b :=
 begin
   intros h1 h2 h3,
   scratch_tac,
--- src/robot.lean
+++ src/robot.lean
@@ -8,38 +8,45 @@ universe u
 
 namespace robot
 
+meta structure statement :=
+(body : expr)
+(type : expr)
+(refs : table expr)
+
 meta inductive hyp_telescope
--- prev are values that have been substituted for the binder in the past.
+-- `prev` are values that have been substituted for the binder in the past.
 |Take  (type : expr) (prev : table expr) (rest : hyp_telescope)
 |Give  (type : expr) (rest : hyp_telescope)
 |Premiss (prop: expr) (rest : hyp_telescope) 
 |Conclusion (prop : expr)
 
-meta structure hyp :=
-    (body : expr) -- the proof term for the hyp
-    (type : hyp_telescope) -- the type has been 'preunwound' to make the conclusion more accessible.
+meta structure hyp extends statement :=
+    (telescope : hyp_telescope) -- the type has been 'preunwound' so that arguments can be tagged with data.
     (vuln : bool) -- if it is vulnerable then it is fair game for delete_dangling and delete_unmatchable
-    (refs : table expr) -- a table of local_consts that the type depends on.
 
-meta inductive bullet_type |Diamond |Circle
+meta inductive bullet_mode |Diamond |Circle
 
-meta structure bullet :=
-    (dependent : table expr) -- local-consts and metavariables that it depends on
+meta structure bullet extends statement :=
+    (dependent : table expr) -- local-consts and metavariables that it is allowed to depend on. This is different to `refs` which tells you the references in the type signature.
     (independent : table expr) -- local-consts and metavariables that it is not allowed to depend on
-    (type : bullet_type)
+    (mode : bullet_mode)
 
+@[inline] meta def target := statement
+@[inline] meta def targets := dict expr target
 meta def hyps := dict expr hyp
+meta def bullets := dict expr bullet
 meta structure robot_state :=
     (vars : list expr)
     (hyps : hyps)
-    (target : expr)
-    (bullets : dict expr bullet)
+    (targets : targets)
+    (bullets : bullets)
     
 @[reducible] meta def robot := state_t robot_state tactic
 
 private meta def mk_hyp_telescope : expr → tactic hyp_telescope
 | (pi n bi a b) := do
-    -- TODO; if `a` is a sigma or a product, convert to a Pi using Exists.elim
+    -- [TODO] if `a` is a sigma or a product, convert to a Pi using Exists.elim
+    -- [TODO] consider using `init/meta/fun_info.lean` to do this.
     p ← is_prop a,
     rest ← mk_hyp_telescope b,
     return $ if p then hyp_telescope.Take a table.empty rest
@@ -54,6 +61,14 @@ private meta def mk_hyp_telescope : expr → tactic hyp_telescope
     end
 | x := return $ hyp_telescope.Conclusion x 
 
+meta def expr.is_mvar : expr → bool
+|(mvar _ _ _) := tt
+|_ := ff
+
+meta def get_refs (e : expr) : table expr :=
+expr.fold e table.empty (λ e' _ es, if expr.is_local_constant e' ∨ expr.is_mvar e' then table.insert e' es else es)
+
+
 private meta def mk_hp_vars_and_hyps : (list expr) → hyps → (list expr) → tactic ((list expr) × hyps)
 |(vs) (hs) ([]) := return ⟨vs, hs⟩
 |(vs) (hs) (v :: t) := do
@@ -61,32 +76,45 @@ private meta def mk_hp_vars_and_hyps : (list expr) → hyps → (list expr) → 
     type ← infer_type v,
     isProp ← is_prop type,
     if (isProp) then do
-        refs ← return $ table.from_list $ expr.list_local_const $ type,
+        refs ← return $ get_refs type,
         telescope ← mk_hyp_telescope type,
-        hyp ← return $ hyp.mk v telescope false refs,
+        hyp ← return $ {hyp . body :=  v, type:=type, telescope := telescope, vuln:=false, refs := refs},
         mk_hp_vars_and_hyps vs (dict.insert v hyp hs) t 
     else mk_hp_vars_and_hyps (v :: vs) hs t
 
+private meta def mk_goals_and_bullets : list expr → tactic (targets × bullets)
+| [] := pure ⟨empty, empty⟩
+| (g :: gs) := do
+    type ← infer_type g,
+    refs ← get_refs type,
+    isProp ← is_prop type,
+    ⟨ts,bs⟩ ← mk_goals_and_bullets gs,
+    if isProp then 
+        let targ := {statement . body:=g, type:=type, refs:=refs} in
+        pure (⟨dict.insert targ ts, bs⟩ : targets × bullets)
+    else 
+        let b := {bullet . body:= g, type:=type, refs:=refs, depends}
+
 private meta def mk_robot_state : tactic robot_state := do
     ctx ← local_context,
-    t ← target,
+    gs ← get_goals,
     ⟨vs, hs⟩ ← mk_hp_vars_and_hyps [] empty ctx,
     return $ {vars := vs, hyps := hs, target := t, bullets := empty }
 
 meta def get_hyps : robot hyps := robot_state.hyps <$> get
 meta def set_hyps (hs:hyps) : robot unit := modify $ λ s, {hyps := hs, ..s}
 meta def get_vars : robot $ list expr := robot_state.vars <$> get
+meta def get_targets : robot targets := robot_state.targets <$> get
 private meta def of_tactic {α : Type} : tactic α → robot α := state_t.lift --[HACK] I get weird name clash errors if I call it `lift`.
 -- meta instance {α : Type} : has_coe (tactic α) (robot α) := ⟨lift⟩ 
 meta def run {α : Type} (r : robot α) : tactic α := prod.fst <$> (mk_robot_state >>= r.run)
 
-/--Remove a hypothesis. -/
+/--Remove a hypothesis from the tactic_state and robot_state. -/
 meta def clear_hyp (e : expr) : robot unit := do
     hs ← get_hyps,
     hs' ← pure $ dict.filter (λ _ (h: hyp), decidable.to_bool $ e ≠ h.body) $ hs,
     of_tactic $ clear e,
     set_hyps hs'
 
-    
 
 end robot
